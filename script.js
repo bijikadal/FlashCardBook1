8e15347326e82e953b1d2f1e50ced9d558c5731d
bijikadal <139132394+bijikadal@users.noreply.github.com>
2025-04-21 16:54:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,493 @@
+const flashcards = {
+    bab1: [
+        { kr: "존경을 나타내다", id: "menunjukkan rasa hormat" },
+        { kr: "어리다", id: "muda" },
+        { kr: "한편", id: "di sisi lain" },
+        { kr: "공적 상황", id: "situasi resmi" },
+        { kr: "다만", id: "namun" },
+        { kr: "받치다", id: "menyangga" }
+    ],
+    bab2: [
+        { kr: "대형", id: "besar" },
+        { kr: "식료품", id: "bahan makanan" },
+        { kr: "통해", id: "melalui" },
+        { kr: "추가 할인을 받다", id: "mendapat diskon tambahan" },
+        { kr: "또한", id: "selain itu" },
+        { kr: "이르기까지", id: "hingga / sampai" },
+        { kr: "직거래", id: "transaksi langsung" },
+        { kr: "이루어지다", id: "terjadi" }
+    ],
+    bab3: [
+        { kr: "주변", id: "sekitar" },
+        { kr: "관문", id: "gerbang" },
+        { kr: "수행하다", id: "melaksanakan" },
+        { kr: "효율적", id: "efisien" },
+        { kr: "인근", id: "sekitar" },
+        { kr: "지나가다", id: "lewat" },
+        { kr: "과학", id: "ilmu pengetahuan" },
+        { kr: "세계적 규모", id: "skala global" },
+        { kr: "주선", id: "perkapalan" },
+        { kr: "공업 발달", id: "perkembangan industri" },
+        { kr: "이 외에도", id: "selain itu" },
+        { kr: "섬유", id: "tekstil" }
+    ],
+    bab4: [
+        { kr: "마루", id: "ruang tamu" },
+        { kr: "휴식", id: "istirahat" },
+        { kr: "취하다", id: "memilih / mengambil keputusan" },
+        { kr: "소통하다", id: "berkomunikasi / terhubung" },
+        { kr: "현대", id: "modern" },
+        { kr: "지속되다", id: "bertahan" },
+        { kr: "깔다", id: "menyebar" },
+        { kr: "순환시키는 방식", id: "cara mengalirkan" }
+    ],
+    bab5: [
+        { kr: "양력", id: "matahari (penanggalan Masehi)" },
+        { kr: "큰 숫자", id: "angka besar" },
+        { kr: "적히다", id: "tertulis" },
+        { kr: "표시하다", id: "menunjukkan / penanda" },
+        { kr: "정부 기관", id: "lembaga pemerintah" },
+        { kr: "광복절", id: "hari kemerdekaan" },
+        { kr: "국경일", id: "hari libur nasional" },
+        { kr: "법정 공휴일", id: "hari libur resmi" },
+        { kr: "새해 첫날", id: "hari tahun baru" },
+        { kr: "종교", id: "kepercayaan / agama" },
+        { kr: "불교/부처", id: "Budha" },
+        { kr: "기준", id: "dasar acuan" },
+        { kr: "쓰이다", id: "dipakai" }
+    ],
+    bab6: [
+        { kr: "직장인의 하루", id: "keseharian pekerja kantoran" },
+        { kr: "대부분", id: "biasanya" },
+        { kr: "대중교통", id: "umum" },
+        { kr: "개인 승용차", id: "mobil pribadi" },
+        { kr: "혼잡하다", id: "macet" },
+        { kr: "예기치 않은", id: "tidak terduga" },
+        { kr: "격다", id: "mengalami" },
+        { kr: "구내식당", id: "kantin perusahaan" },
+        { kr: "부서", id: "departemen" },
+        { kr: "즐기다", id: "menikmati" },
+        { kr: "쌓인 피로", id: "menumpuk kelelahan" },
+        { kr: "다지기 위해", id: "untuk memperkuat" },
+        { kr: "어떤 사람", id: "beberapa orang" },
+        { kr: "자기 개발", id: "pengembangan diri" },
+        { kr: "기상", id: "kondisi saat bangun tidur" },
+        { kr: "취침", id: "waktu tidur (formal)" },
+        { kr: "하루", id: "24 jam sehari (12-12)" },
+        { kr: "일과", id: "jadwal" }
+    ],
+    bab7: [
+        { kr: "뚜렷하다", id: "jelas terlihat" },
+        { kr: "벛꽃", id: "bunga sakura" },
+        { kr: "봄맞이", id: "penyambutan musim semi" },
+        { kr: "잠을 설치다", id: "tidur terganggu (solusi pemasangan)" },
+        { kr: "밤에는 열대야로", id: "panas tropis (malam sangat panas)" },
+        { kr: "이어지다", id: "berlanjut / terus menerus" },
+        { kr: "형형색색", id: "berbagai warna" },
+        { kr: "곱게", id: "dengan penampilan indah" },
+        { kr: "물든", id: "yang berubah warna" },
+        { kr: "단풍을 보기 위해", id: "untuk melihat daun" },
+        { kr: "떠나다", id: "pergi" },
+        { kr: "축제", id: "festival" },
+        { kr: "얼다", id: "membeku" },
+        { kr: "높은 지대", id: "daerah yang tinggi" },
+        { kr: "현상", id: "fenomena" },
+        { kr: "즐기다", id: "menikmati" },
+        { kr: "경치가", id: "pemandangan" },
+        { kr: "그러나", id: "namun" },
+        { kr: "짧아진 것 같다고", id: "katanya semakin pendek" }
+    ],
+    bab8: [
+        { kr: "합격하다", id: "lulus" }
+    ],
+    makanan: [
+        { kr: "구성된 식사", id: "makanan yang terdiri dari" },
+        { kr: "주식으로 쌀", id: "nasi adalah makanan pokok" },
+        { kr: "다양하다", id: "berbagai macam" },
+        { kr: "곁들이다", id: "melengkapi" },
+        { kr: "나누다", id: "membagi" },
+        { kr: "소중하다", id: "menghargai / berharga" },
+        { kr: "최근", id: "baru-baru ini" },
+        { kr: "증가하다", id: "meningkat" },
+        { kr: "결제", id: "pembayaran" },
+        { kr: "완료되면", id: "jika selesai" },
+        { kr: "출력되다", id: "dicetak" },
+        { kr: "자세하다", id: "dengan rinci" }
+    ],
+    toko: [
+        { kr: "자동화", id: "otomatis" },
+        { kr: "결제하다", id: "melakukan pembayaran" },
+        { kr: "운영", id: "dioperasikan" },
+        { kr: "고급화", id: "kecanggihan" },
+        { kr: "생수", id: "air mineral" },
+        { kr: "문구점", id: "alat tulis" },
+        { kr: "특정", id: "tertentu" },
+        { kr: "취급하다", id: "menangani / menjual" },
+        { kr: "이 외도", id: "selain itu juga" },
+        { kr: "이러한", id: "seperti ini" },
+        { kr: "또한", id: "juga" },
+        { kr: "장벽", id: "penghalang" },
+        { kr: "간편하게", id: "dengan mudah / praktis" }
+    ],
+    sampah: [
+        { kr: "헌옷", id: "pakaian bekas" },
+        { kr: "대형", id: "ukuran besar" },
+        { kr: "종량제", id: "sistem perhitungan" },
+        { kr: "최대한도", id: "batas maksimum" },
+        { kr: "최대", id: "tertinggi / terbanyak" },
+        { kr: "제고하다", id: "meningkat" },
+        { kr: "닭뼈나 달걀 껍질", id: "tulang ayam atau kulit telur" },
+        { kr: "수거", id: "pengumpulan" },
+        { kr: "빈병", id: "botol kosong" },
+        { kr: "캔", id: "kaleng" },
+        { kr: "담요", id: "selimut" }
+    ],
+    transportasi: [
+        { kr: "뿐만 아니라", id: "tidak hanya" },
+        { kr: "이동하다", id: "pergerakan" },
+        { kr: "연결하는 노선", id: "menghubungkan rute" },
+        { kr: "주요", id: "utama" },
+        { kr: "운행", id: "pengoperasian" },
+        { kr: "거리", id: "jarak" },
+        { kr: "최남단 도시", id: "kota paling selatan" },
+        { kr: "이동할 수 있습니다", id: "bisa bergerak" },
+        { kr: "교통수단", id: "sarana transportasi" },
+        { kr: "통해", id: "melalui" }
+    ],
+    agama: [
+        { kr: "다종교", id: "semua agama" },
+        { kr: "개신교", id: "protestan" },
+        { kr: "천주교", id: "katolik" },
+        { kr: "신자", id: "umat" },
+        { kr: "비율", id: "rasio" },
+        { kr: "신앙생활을 하다", id: "kehidupan beragama" }
+    ],
+    alamat: [
+        { kr: "주소를 표기하다", id: "mencatat jalan" },
+        { kr: "위의 예를 보면 알 수 있다", id: "bisa tahu jika melihat contoh di atas" },
+        { kr: "구성되다", id: "terdiri dari" },
+        { kr: "짝수로 표시하다", id: "menunjukkan angka genap" }
+    ],
+    pakaian: [
+        { kr: "의상", id: "pakaian" },
+        { kr: "화려하며", id: "mewah / mencolok" },
+        { kr: "우아하다", id: "anggun" },
+        { kr: "조끼로", id: "rompi" },
+        { kr: "자연", id: "natural" },
+        { kr: "풍성하다", id: "tebal" },
+        { kr: "어울리다", id: "berbaur / cocok" },
+        { kr: "장점", id: "kelebihan" },
+        { kr: "다소", id: "agak / sedikit / kecil" },
+        { kr: "점이", id: "hal" },
+        { kr: "개량하다", id: "perbaikan (penyempurnaan)" },
+        { kr: "단순하고 실용적", id: "sederhana dan praktis" },
+        { kr: "푸른색", id: "warna terang" },
+        { kr: "노력", id: "usaha" }
+    ],
+    perumahan: [
+        { kr: "보안", id: "keamanan" },
+        { kr: "갖추어", id: "dilengkapi" },
+        { kr: "증가하다", id: "meningkat" },
+        { kr: "구비되다", id: "dilengkapi" },
+        { kr: "필수적", id: "wajib" },
+        { kr: "점이", id: "hal" },
+        { kr: "부족하다", id: "kekurangan" }
+    ],
+    budaya: [
+        { kr: "역사", id: "sejarah" },
+        { kr: "풍부하다", id: "kaya, banyak, cukup" },
+        { kr: "시대", id: "generasi" },
+        { kr: "보존되다", id: "dilestarikan" },
+        { kr: "건축 양식", id: "bentuk bangunan" },
+        { kr: "비원", id: "taman indah" },
+        { kr: "어우러지다", id: "menyatu" },
+        { kr: "더욱", id: "lebih" },
+        { kr: "보전되다", id: "terjaga, terpelihara" },
+        { kr: "상징하다", id: "melambangkan" },
+        { kr: "돌", id: "batu" },
+        { kr: "불상", id: "patung Buddha" },
+        { kr: "뛰어난 기술과 예술적 감각을 엿볼 수", id: "menampilkan teknologi canggih dan kepekaan artistik masyarakat Silla" },
+        { kr: "빼놓다", id: "pengecualian, mencabut, melewatkan" },
+        { kr: "유교", id: "konghucu" },
+        { kr: "고스란히", id: "seperti aslinya" },
+        { kr: "가옥", id: "rumah, tempat tinggal" },
+        { kr: "개드로", id: "seperti itu" },
+        { kr: "정원", id: "taman" },
+        { kr: "감각", id: "rasa" },
+        { kr: "미적", id: "estetika" },
+        { kr: "보여주다", id: "menunjukkan" }
+    ],
+    waktu: [
+        { kr: "탄력적", id: "fleksibel" },
+        { kr: "유연근무제", id: "kerja fleksibel" },
+        { kr: "확대되다", id: "diperluas" },
+        { kr: "실시하다", id: "melaksanakan" },
+        { kr: "만족하다", id: "puas" },
+        { kr: "잡담", id: "obrolan ringan" },
+        { kr: "균형", id: "keseimbangan" },
+        { kr: "중시하다", id: "mengutamakan" },
+        { kr: "사회적", id: "sosial" },
+        { kr: "확산하다", id: "menyebar" },
+        { kr: "질", id: "kualitas, mutu" },
+        { kr: "관심", id: "minat, bakat" },
+        { kr: "관하다", id: "mengenai" },
+        { kr: "전문", id: "professional, seluruh isi" },
+        { kr: "서적", id: "buku, bacaan" },
+        { kr: "출간/출판", id: "diterbitkan" },
+        { kr: "학과", id: "jurusan" },
+        { kr: "개설되다", id: "dibuka" },
+        { kr: "여럿", id: "banyak orang" },
+        { kr: "증가하다", id: "diperluas, bertambah" }
+    ],
+    khusus: [
+        { kr: "원래", id: "awalnya" },
+        { kr: "은혜를 기억하다", id: "mengingat jasa" },
+        { kr: "찹쌀", id: "beras ketan" },
+        { kr: "기운이 없을 때", id: "ketika tidak ada energi" },
+        { kr: "흐리다", id: "mengucur" },
+        { kr: "둘러앉아", id: "duduk bersama" },
+        { kr: "계란 지단이나 김", id: "potongan telur" },
+        { kr: "배달 앱", id: "apk pengantar" }
+    ],
+    aplikasi : [
+        { kr: "등장으로", id: "kemunculan" },
+        { kr: "더욱", id: "semakin" },
+        { kr: "정보를 손쉽게", id: "informasi dengan mudah" },
+        { kr: "평점과", id: "skor" },
+        { kr: "참고하다", id: "merujuk" },
+        { kr: "게다가", id: "selain itu" },
+        { kr: "지원하다", id: "mendukung" },
+        { kr: "현재", id: "saat ini" },
+        { kr: "혜택", id: "manfaat" },
+        { kr: "실시간", id: "part time" },
+        { kr: "배달 중인지", id: "sedang diantar" },    
+    ],
+    medis :[
+        { kr: "의료기관", id: "lembaga medis" },
+        { kr: "외래 환자", id: "rawat jalan" },
+        { kr: "동네 의원", id: "komplek klinik" },
+        { kr: "병상", id: "tempat tidur RS" },
+        { kr: "증상", id: "gejala" },
+        { kr: "발행하다", id: "menerbitkan" },
+        { kr: "급여", id: "gaji/keuntungan" },
+        { kr: "적용", id: "penerapan" },
+        { kr: "진료", id: "pengobatan" } 
+    ],
+    obatobatan:[
+        { kr: "조제하다", id: "meracik" },
+        { kr: "복용 방법", id: "cara konsumsi" },
+        { kr: "명칭", id: "nama sebutan" },
+        { kr: "이때는", id: "saat ini" },
+        { kr: "지역별", id: "berdasarkan wilayah" },
+        { kr: "안전상비의약품", id: "obat darurat" },
+        { kr: "해열제", id: "penurun panas" },
+        { kr: "진통제", id: "pereda nyeri" },
+        { kr: "감기약", id: "flu" },
+        { kr: "소화제", id: "pencernaan" },
+        { kr: "지사제", id: "anti diare" },
+        { kr: "파스", id: "koyo" }
+    ],
+    bab23:[
+        { kr: "문서", id: "dokumen" },
+        { kr: "특급", id: "ekspres" },
+        { kr: "전달할 수", id: "bisa mengantarkan" },
+        { kr: "배달 상황", id: "status pengiriman" },
+        { kr: "조회할 수", id: "bisa mengecek" },
+        { kr: "선편", id: "pengiriman laut" },
+        { kr: "물론", id: "tentu saja" },
+        { kr: "동식물", id: "hewan dan tumbuhan" },
+        { kr: "고가", id: "harga tinggi" },
+        { kr: "수령 확인 서비스", id: "layanan konfirmasi penerima" },
+        { kr: "수령 사실", id: "hasil penerimaan" },
+        { kr: "한편", id: "selain itu" },
+        { kr: "납부하다", id: "membayar" }        
+    ],
+    bab24:[
+        { kr: "외환 거래", id: "transaksi valuta asing" },
+        { kr: "금융 서비스", id: "layanan keuangan" },
+        { kr: "이체되다", id: "transfer" },
+        { kr: "증명하다", id: "membuktikan" },
+        { kr: "해당", id: "terkait" },
+        { kr: "사항", id: "poin/pemberitahuan" },
+        { kr: "구체적", id: "spesifik" },
+        { kr: "적합하다", id: "sesuai/cocok" }
+    ],
+    bab25:[
+        { kr: "언어소통", id: "komunikasi bahasa" },
+        { kr: "설립하다", id: "mendirikan" },
+        { kr: "생활 적응", id: "adaptasi kehidupan" },
+        { kr: "겪다", id: "mengalami" },
+        { kr: "농촌 지역", id: "daerah pedesaan" },
+        { kr: "체계적", id: "terstruktur" },
+        { kr: "효율적", id: "efisien" },
+        { kr: "두축", id: "pembangunan" }, // bisa juga "dua pilar" jika konteksnya lebih spesifik
+        { kr: "꾸준하다", id: "terus menerus" }     
+    ],
+    bab26:[
+        { kr: "예외", id: "pengecualian" },
+        { kr: "밥상", id: "meja hidangan" },
+        { kr: "중앙", id: "tengah" },
+        { kr: "마치다", id: "selesai" },
+        { kr: "동시", id: "bersamaan" },
+        { kr: "씹다", id: "mengunyah" }        
+    ],
+    bab27:[
+        { kr: "국경일", id: "hari nasional" },
+        { kr: "경축하다", id: "merayakan" },
+        { kr: "독립", id: "kemerdekaan" },
+        { kr: "만세", id: "merdeka" },
+        { kr: "해방되다", id: "dibebaskan" },
+        { kr: "식민 지배", id: "penjajah kolonial" },
+        { kr: "창제", id: "menciptakan" },
+        { kr: "국기", id: "bendera" },
+        { kr: "게양하다", id: "mengibarkan" },
+        { kr: "일생", id: "kehidupan" },
+        { kr: "애도하다", id: "berduka" },
+        { kr: "친지들", id: "para kerabat" },
+        { kr: "염원하다", id: "mendoakan" },
+        { kr: "일생의례", id: "upacara kehidupan (pernikahan)" },
+        { kr: "풍습", id: "tradisi" },
+        { kr: "지인", id: "kenalan" },
+        { kr: "치르다", id: "mengadakan" },
+        { kr: "조문객", id: "tamu" },
+        { kr: "묵념을 하다", id: "momen hening" },
+        { kr: "위로하다", id: "menghibur" },
+        { kr: "조상", id: "leluhur" },
+        { kr: "지내다", id: "melaksanakan" },
+        { kr: "형식", id: "bentuk" },
+        { kr: "추모", id: "mengenang" },
+        { kr: "여기다", id: "menganggap" }     
+    ],
+    bab28:[
+        { kr: "뜨다", id: "membuka mata" },
+        { kr: "가지", id: "macam" },
+        { kr: "곡식", id: "biji dan padi-padian" },
+        { kr: "갖가지", id: "berbagai macam" },
+        { kr: "마른 나물", id: "sayuran kering" },
+        { kr: "땅콩이나 호두", id: "kacang tanah / kenari" },
+        { kr: "견과류", id: "kacangan dan biji-bijian" },
+        { kr: "부럼", id: "kacang kulit keras" },
+        { kr: "피부병", id: "penyakit kulit" },
+        { kr: "달빛", id: "cahaya bulan" },
+        { kr: "설날", id: "Tahun Baru Imlek (bulan 1 tanggal 12 menurut penanggalan lunar)" },
+        { kr: "정월대보름", id: "Hari Bulan Purnama Pertama (bulan 1 tanggal 15 lunar)" },
+        { kr: "추석", id: "Hari Panen (bulan 8 tanggal 15 lunar)" }        
+    ],
+    bab29:[
+        { kr: "조화롭다", id: "harmoni" },
+        { kr: "약자", id: "orang yang rentan" },
+        { kr: "이웃", id: "tetangga" },
+        { kr: "더욱", id: "lebih" },
+        { kr: "신경을 쓰다", id: "memperhatikan/peduli" },
+        { kr: "되도록", id: "sebisa mungkin" },
+        { kr: "행위", id: "tindakan" },
+        { kr: "미술관", id: "galeri seni" },
+        { kr: "줄", id: "barisan" },
+        { kr: "어르신", id: "lansia" },
+        { kr: "자세", id: "sikap" }        
+    ],
+    bab30:[
+        { kr: "매력", id: "daya tarik" },
+        { kr: "역할", id: "peran" },
+        { kr: "무대", id: "panggung" },
+        { kr: "화려하다", id: "megah" },
+        { kr: "흥미진진하다", id: "sangat menarik" },
+        { kr: "연기", id: "akting" },
+        { kr: "영사", id: "visual / proyeksi film" },
+        { kr: "작품", id: "karya / ciptaan" }
+    ]
+  };
+  
+  // Inisialisasi dropdown
+  const themeSelect = document.getElementById("theme-select");
+  const container = document.getElementById("flashcard-container");
+  
+  const labels = {
+    bab1: "1.한국의 인사 예절 etika salam dikorea",
+    bab2: "2.한국에서 알뜰하게 생필품을 구입하는 팁",
+    bab3: "3.한국의 도시 kota di korea",
+    bab4: "4.한국의 좌식 문화 budaya korea duduk di lantai",
+    bab5: "5.양력과 음력 kalender bulan dan matahari",
+    bab6: "6.직장인의 하루 keseharian pekerja kantoran",
+    bab7: "7.날씨 cuaca",
+    bab8: "8.높임말과 반말",
+    makanan: "9.한훅인의 식사 makanan korea",
+    toko: "10.무인 판매점 toko tanpa pegawai",
+    sampah: "11.쓰레기 분리배출 pemilihan sampah",
+    transportasi: "12.한국의 대중교통 transportasi umum",
+    agama: "13.한국인의 종교 agama orang korea",
+    alamat: "14.도로명 주소 alamat nama jalan",
+    pakaian: "15.한국의 전통 의사 pakaian tradisional korea",
+    perumahan: "16.한국의 주거 형태 type perumaahan korea",
+    budaya: "17.한국의 문화유산 warisan budaya korea",
+    waktu: "18.한국인의 여가 생활 kehidupan waktu senggang",
+    khusus: "19.특별한 날 먹는 음식 yg dimakan pada hari khusus",
+    aplikasi: "20.배달 앱 apk pengantar",
+    medis: "21.한국의 의료체계",
+    obatobatan: "22.의약품 구입 membeli obat-obatan",
+    bab23: "23.한국의 우체국 서비스 layanan pos korea",
+    bab24: "24.한국의 은행 bank korea",
+    bab25: "25.외국인력상담센타 pusat bantuan tenaga kerja asing",
+    bab26: "26.한국의 식사 여절 etika makan dikorea",
+    bab27: "27.한국의 특별한날 hari sepesial dikorea",
+    bab28: "28.한국의 명절 korean tradisional Holiday",
+    bab29: "29.공공생활 etika ditempat umum",
+    bab30: "30.한류 문화 budaya hallyu"
+  };
+  
+  for (let key in flashcards) {
+    const option = document.createElement("option");
+    option.value = key;
+    option.textContent = labels[key] || key;
+    themeSelect.appendChild(option);
+  }
+  
+  themeSelect.addEventListener("change", () => {
+    const selected = themeSelect.value;
+    renderFlashcards(flashcards[selected]);
+  });
+  function renderFlashcards(cards) {
+    container.innerHTML = "";
+    cards.forEach((card, index) => {
+      const cardDiv = document.createElement("div");
+      cardDiv.className = "card";
+      cardDiv.innerHTML = `
+        <div class="card-inner">
+          <div class="card-front">${card.kr}</div>
+          <div class="card-back">${card.id}</div>
+        </div>
+      `;
+  
+      cardDiv.addEventListener("click", () => {
+        cardDiv.classList.toggle("flipped");
+      });
+  
+      container.appendChild(cardDiv);
+    });
+  }
+  // Tampilkan tema pertama saat awal load
+  themeSelect.value = "bab1";
+  renderFlashcards(flashcards.bab1);
+  
+  // Shuffle
+document.getElementById("shuffle-btn").addEventListener("click", () => {
+    const selected = themeSelect.value;
+    const shuffled = [...flashcards[selected]].sort(() => Math.random() - 0.5);
+    renderFlashcards(shuffled);
+  });
+  
+  // Toggle semua jawaban
+  let allFlipped = false;
+  document.getElementById("toggle-all-btn").addEventListener("click", () => {
+    allFlipped = !allFlipped;
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+      if (allFlipped) {
+        card.classList.add("flipped");
+      } else {
+        card.classList.remove("flipped");
+      }
+    });
+    document.getElementById("toggle-all-btn").textContent = allFlipped ? "Sembunyikan Semua" : "Tampilkan Semua";
+  });
+  
